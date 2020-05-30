3ba06a5fce71deb4ae75ebe72a51c64d64368e22
Marcin Pogorzelski <marcin.pogorzelski@embeddev.se>
2020-05-30 11:24:45 +0200

    Add try except around regular market price

--- yfinance/base.py
+++ yfinance/base.py
@@ -324,7 +324,11 @@ class TickerBase():
             if isinstance(data.get(item), dict):
                 self._info.update(data[item])
 
-        self._info['regularMarketPrice'] = self._info['regularMarketOpen']
+        try:
+            self._info['regularMarketPrice'] = self._info['regularMarketOpen']
+        except:
+            self._info['regularMarketPrice'] = 0
+            
         self._info['logo_url'] = ""
         try:
             domain = self._info['website'].split(
